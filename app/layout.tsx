02d9c52ae30b8f6236e1d0323042e5d1c02a139c
Felipe Chaves <felipecchaves@gmail.com>
2024-04-17 09:39:06 -0300

    background

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased bg-neutral-600`}>
+      <body className={`${inter.className} antialiased`}>
         {children}
       </body>
     </html>
